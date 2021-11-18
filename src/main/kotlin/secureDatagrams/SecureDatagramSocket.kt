1db57e59d704637520b95d768db5dbaf4f393321
André Palma <andremarquespalma@gmail.com>
2021-11-18 17:06:05 +0000

    Init Ciphers Once

--- src/main/kotlin/secureDatagrams/SecureDatagramSocket.kt
+++ src/main/kotlin/secureDatagrams/SecureDatagramSocket.kt
@@ -11,37 +11,43 @@ import javax.crypto.spec.IvParameterSpec
 
 class SecureDatagramSocket : DatagramSocket {
 
-    private lateinit var cipher: Cipher
-    private lateinit var key: SecretKey
+    private val encryptCipher: Cipher
+    private val decryptCipher: Cipher
+    private val key: SecretKey
 
-    constructor(a: SocketAddress) : super(a) {
-        init()
-    }
-
-    constructor() : super() {
-        init()
-    }
-
-
-    private fun init() {
-        val kg: KeyGenerator = KeyGenerator.getInstance("AES")
+    init {
+        val kg = KeyGenerator.getInstance("AES")
         kg.init(SecureRandom(Settings.symPassword.toByteArray()))
         key = kg.generateKey()
-        cipher = Cipher.getInstance(Settings.symmetricSuite)
+        encryptCipher = Cipher.getInstance(Settings.symmetricSuite)
+        encryptCipher.init(Cipher.ENCRYPT_MODE, key, IvParameterSpec(Settings.iv))
+        decryptCipher = Cipher.getInstance(Settings.symmetricSuite)
+        decryptCipher.init(Cipher.DECRYPT_MODE, key, IvParameterSpec(Settings.iv))
     }
 
+    constructor(a: SocketAddress) : super(a)
+
+    constructor() : super()
+
+    /**
+     * Unlike its parent implementation, the [DatagramPacket.buf] is replaced by a new one
+     * since the old one will contain encrypted data
+     * @see DatagramSocket.send
+     */
     override fun send(p: DatagramPacket) {
-        // encryption
 //        println("${String(p.data,0,p.length)} ${p.length}")
-        cipher.init(Cipher.ENCRYPT_MODE, key, IvParameterSpec(Settings.iv))
-        p.data = cipher.doFinal(p.data, 0, p.length)
+        p.data = encryptCipher.doFinal(p.data, 0, p.length)
         super.send(p)
     }
 
+    /**
+     * Unlike its parent implementation, the [DatagramPacket.buf] is replaced by a new one
+     * since the old one will contain encrypted data
+     * @see DatagramSocket.receive
+     */
     override fun receive(p: DatagramPacket) {
         super.receive(p)
 //        println("${String(p.data,0,p.length)} ${p.length}")
-        cipher.init(Cipher.DECRYPT_MODE, key, IvParameterSpec(Settings.iv))
-        p.data = cipher.doFinal(p.data, 0, p.length)
+        p.data = decryptCipher.doFinal(p.data, 0, p.length)
     }
 }
